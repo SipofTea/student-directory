c1457f915a987ded9d3067719667f5c5c18b66e7
SipofTea <p.e.meijerink@gmail.com>
2021-06-15 11:38:19 +0000

    Added some comments to the directory file

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# prints a list of all students
 puts "The students of Villains Academy"
 puts "--------------------------------"
 puts "Dr. Hannibal Lecter"
@@ -11,7 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-
+# prints the number of students
 print "Overall, we have "
 print 11
 puts " great students"
