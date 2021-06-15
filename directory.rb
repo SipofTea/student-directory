c2256dd0ee9964c7f6a6b96204b4bec9698cb590
SipofTea <p.e.meijerink@gmail.com>
2021-06-15 18:47:38 +0000

    Added handling  user input to create student array instead of preset array

--- directory.rb
+++ directory.rb
@@ -1,18 +1,18 @@
 
-# array of all students
-students_array = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november},  
-  ]
+# Until the end of week 4 step 6
+# students_array = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex DeLarge", cohort: :november},
+#   {name: "The Wicked Witch of the West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november},  
+#   ]
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +29,29 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
 
+# print_header
+# print(students_array)
+# print_footer(students_array)
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  
+  students = []
+  
+  name = gets.chomp
+  
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  
+  students
+end
+
+current_students = input_students
+
 print_header
-print(students_array)
-print_footer(students_array)
+print(current_students)
+print_footer(current_students)
