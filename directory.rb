c275baefc8ecb4e0490fc233b904e72e89b0da88
SipofTea <61120785+SipofTea@users.noreply.github.com>
2021-06-17 13:21:56 +0000

    Added an interactive menu and refactored so that functions have their own methods

--- directory.rb
+++ directory.rb
@@ -13,45 +13,64 @@
 #   {name: "Joffrey Baratheon", cohort: :november},
 #   {name: "Norman Bates", cohort: :november},  
 #   ]
+@students = []
 
 def print_header
   puts "The students of Villains Academy"
   puts "--------------------------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
   puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
-
-# print_header
-# print(students_array)
-# print_footer(students_array)
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  
-  students = []
-  
   name = gets.chomp
-  
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  
-  students
 end
 
-current_students = input_students
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-print_header
-print(current_students)
-print_footer(current_students)
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def excecute_choice_process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "Please choose a number from the menu"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    excecute_choice_process(gets.chomp)
+  end  
+end
+
+interactive_menu
