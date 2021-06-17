892d388d784df2896afef81f093787f79cff7049
SipofTea <61120785+SipofTea@users.noreply.github.com>
2021-06-17 13:38:39 +0000

    Added functionality for saving list of students

--- directory.rb
+++ directory.rb
@@ -44,6 +44,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list of students to students.csv"
   puts "9. Exit"
 end
 
@@ -53,12 +54,24 @@ def show_students
   print_footer
 end
 
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_entry = student_data.join(",")
+    file.puts csv_entry
+  end
+  file.close
+end
+
 def excecute_choice_process(selection)
   case selection
     when "1"
       students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -72,5 +85,7 @@ def interactive_menu
     excecute_choice_process(gets.chomp)
   end  
 end
+
+
 
 interactive_menu
