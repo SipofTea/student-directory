0c4e5add41bea40a7ecdc88437d891fb311add59
SipofTea <61120785+SipofTea@users.noreply.github.com>
2021-06-20 15:14:31 +0000

    students.csv is now loaded on startup unless other file is given

--- directoryexercises2.rb
+++ directoryexercises2.rb
@@ -54,21 +54,22 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
+def load_students(filename)
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     add_student(name, cohort)
   end
+  puts "Loaded #{@students.count} from #{filename}" 
   file.close
 end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    load_students("students.csv")
+  elsif File.exist?(filename)
     load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"  
   else
     puts "Sorry, #{filename} doesn't exist."
     exit
